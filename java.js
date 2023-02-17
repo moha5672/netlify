34657c15f21f6e5eb7355d6708c9395d092693fb
Mohamad Hussein <hammoudi5672@gmail.com>
2023-02-17 14:10:40 +0100

    java

---
+++ java.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var meretext = document.getElementById("mere");
+  var tekstknap = document.getElementById("læsmereknap");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    tekstknap.innerHTML = "Læs mere";
+    meretext.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    tekstknap.innerHTML = "⇱";
+    meretext.style.display = "inline";
+  }
+}
+function startSide() {}
